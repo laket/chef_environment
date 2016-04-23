455c1b7ba4b9a82fed4490881ddf0aaa4536d2c2
laket72 <laket72@gmail.com>
2016-04-23 16:31:57 +0900

    余計なパッケージを削除

--- site-cookbooks/rust/templates/default/rust.el
+++
@@ -1,8 +0,0 @@
-(add-to-list 'load-path (locate-user-emacs-file "rust-mode"))
-(autoload 'rust-mode "rust-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-
-(setq racer-rust-src-path "<%= @rustc_srcdir  =>")
-(setq racer-cmd "<%= @racer_cmd_path  =>")
-(add-to-list 'load-path "<%= @racer_editors =>")
-;(eval-after-load "rust-mode" '(require 'racer))
